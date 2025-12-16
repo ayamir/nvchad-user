04f6b739fab656f7e514a760f17f1af3c7b85e90
ayamir <lgt986452565@gmail.com>
2025-12-16 15:46:49 +0800

    feat: support C-x for fzf picker

--- lua/configs/fzf-lua.lua
+++ lua/configs/fzf-lua.lua
@@ -1,5 +1,15 @@
 return function()
   require("fzf-lua").setup({
     { "telescope" },
+    winopts = {
+      on_create = function()
+        -- use <C-x> to split
+        vim.keymap.set("t", "<C-x>", "<C-x>", {
+          buffer = true,
+          noremap = true,
+          silent = true,
+        })
+      end,
+    },
   })
 end
