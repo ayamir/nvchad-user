70a61a54f398c78116c0ed743368b1d93f9dd60c
ayamir <lgt986452565@gmail.com>
2026-04-15 21:59:56 +0800

    feat: add vim-matchup
    Change-Id: I111a189925860cedaf3edbf2df8c6348d3e41b8a

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -687,4 +687,13 @@ return {
       "rcarriga/nvim-notify",
     },
   },
+
+  {
+    "andymass/vim-matchup",
+    init = function()
+      vim.g.matchup_transmute_enabled = 1
+      vim.g.matchup_surround_enabled = 1
+      vim.g.matchup_matchparen_offscreen = { method = "popup" }
+    end,
+  },
 }
