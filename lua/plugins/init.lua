8406fe26285c0f6f2d276b21d8c54b010ec8eff8
ayamir <lgt986452565@gmail.com>
2026-04-16 20:04:36 +0800

    feat: readd fff
    Change-Id: I3a9e26164cd6db9a69b2dfefc7337585235f28bb

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -696,4 +696,47 @@ return {
       vim.g.matchup_matchparen_offscreen = { method = "popup" }
     end,
   },
+
+  {
+    "dmtrKovalenko/fff.nvim",
+    build = function()
+      require("fff.download").download_or_build_binary()
+    end,
+    version = "0.5.2",
+    lazy = false,
+    keys = {
+      {
+        "ff", -- try it if you didn't it is a banger keybinding for a picker
+        function()
+          require("fff").find_files()
+        end,
+        desc = "FFFind files",
+      },
+      {
+        "fg",
+        function()
+          require("fff").live_grep()
+        end,
+        desc = "LiFFFe grep",
+      },
+      {
+        "fz",
+        function()
+          require("fff").live_grep({
+            grep = {
+              modes = { "fuzzy", "plain" },
+            },
+          })
+        end,
+        desc = "Live fffuzy grep",
+      },
+      {
+        "fc",
+        function()
+          require("fff").live_grep({ query = vim.fn.expand("<cword>") })
+        end,
+        desc = "Search current word",
+      },
+    },
+  },
 }
