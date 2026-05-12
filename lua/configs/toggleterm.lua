5cb59b0ab63cdf50e2538d68b08ad70f4bd0dfc9
ayamir <lgt986452565@gmail.com>
2026-05-12 20:35:52 +0800

    feat: larger toggleterm

--- lua/configs/toggleterm.lua
+++ lua/configs/toggleterm.lua
@@ -12,10 +12,10 @@ return function()
     float_opts = {
       border = "rounded",
       width = function()
-        return math.floor(vim.o.columns * 0.75)
+        return math.floor(vim.o.columns * 0.8)
       end,
       height = function()
-        return math.floor(vim.o.lines * 0.7)
+        return math.floor(vim.o.lines * 0.8)
       end,
       winblend = 15,
     },
