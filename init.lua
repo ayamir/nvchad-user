d2e4db6aaee69d18b9b22b7191a3dc6ca01272b5
ayamir <lgt986452565@gmail.com>
2026-07-09 17:14:15 +0800

    feat: add config for neovide
    Change-Id: I7de7571820b1880a52b8cd88b7b8dd6c6bc97f26

--- init.lua
+++ init.lua
@@ -1,5 +1,21 @@
 vim.g.base46_cache = vim.fn.stdpath("data") .. "/base46/"
 vim.g.mapleader = " "
+vim.g.neovide_input_macos_option_key_is_meta = "both"
+if vim.g.neovide then
+  local function save()
+    vim.cmd.write()
+  end
+  local function copy()
+    vim.cmd([[normal! "+y]])
+  end
+  local function paste()
+    vim.api.nvim_paste(vim.fn.getreg("+"), true, -1)
+  end
+
+  vim.keymap.set({ "n", "i", "v" }, "<D-s>", save, { desc = "Save" })
+  vim.keymap.set("v", "<D-c>", copy, { silent = true, desc = "Copy" })
+  vim.keymap.set({ "n", "i", "v", "c", "t" }, "<D-v>", paste, { silent = true, desc = "Paste" })
+end
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
